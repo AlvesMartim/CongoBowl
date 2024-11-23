e075460b9f548dbd1d78a00a02e8c4d86dd693d1
Alves924 <moreira.alves.martim@gmail.com>
2024-11-23 03:14:38 +0100

    Commit 2

--- CongoBowl/views.py
+++ CongoBowl/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from django.contrib.auth.hashers import make_password
 
 def index(request):
     return render(request, 'index.html')
@@ -10,7 +12,10 @@ def produits(request):
     return render(request,'produits.html')
 
 def sign_in(request):
-    return render(request,'sign_in')
+    if request.method =='POST':
+        return 1
+    return render(request,'sign_in.html')
 
 def log_in(request):
-    return render(request,'log_in')
+    return render(request,'log_in.html')
+
