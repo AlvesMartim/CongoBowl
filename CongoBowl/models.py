c3caa537562152effdcdd6fc0d6e241bacfda9fc
Alves924 <moreira.alves.martim@gmail.com>
2024-12-11 15:57:36 +0100

    Commit 4 -Connexion configuré

--- CongoBowl/models.py
+++ CongoBowl/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
 class Utilisateur(models.Model):
-    nom = models.CharField(max_length=100)
-    prenom = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    mot_de_passe = models.CharField(max_length=100)
-    date_inscription = models.DateTimeField(auto_now_add=True)
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=100, db_column='nom')
+    prenom = models.CharField(max_length=100, db_column='prenom')
+    pseudo = models.CharField(max_length=100, db_column='pseudo', unique =True)
+    email = models.EmailField(unique=True, db_column='mail')
+    mot_de_passe = models.CharField(max_length=100, db_column='mot_de_passe')
+    date_inscription = models.DateTimeField(auto_now_add=True, db_column='date_inscription')
+
+    USERNAME_FIELD = 'pseudo'
+    REQUIRED_FIELDS = ['mot_de_passe', 'pseudo', 'email']
 
     def __str__(self):
         return f"{self.prenom} {self.nom}"
+
+    class Meta:
+        db_table = "Utilisateur"
--- CongoBowl/views.py
+++ CongoBowl/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render, redirect
+from datetime import datetime
+
+from django.shortcuts import render,redirect
+
+from .forms import SignInForm, LogInForm
 from .models import *
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.hashers import make_password, check_password
+from django.contrib import messages
+
 
 def index(request):
-    return render(request, 'index.html')
+    pseudo = request.session['utilisateur_pseudo']
+    return render(request, 'index.html',{'pseudo':pseudo})
 
 def apropos(request):
     return render(request,'apropos.html')
@@ -13,9 +21,39 @@ def produits(request):
 
 def sign_in(request):
     if request.method =='POST':
-        return 1
-    return render(request,'sign_in.html')
+        form =  SignInForm(request.POST)
+        if form.is_valid():
+            utilisateur = form.save(commit=False)
+            utilisateur.mot_de_passe = make_password(form.cleaned_data['mot_de_passe']) #Hashage du mdp
+            utilisateur.save() # Sauv dans la BD
+            request.session['utilisateur_pseudo'] = utilisateur.pseudo
+            request.session['utilisateur_mot_de_passe'] = utilisateur.mot_de_passe
+            request.session['utilisateur_id'] = utilisateur.id
+            print(utilisateur.id)
+            return redirect('index')
+    else :
+        form = SignInForm()
+    return render(request,'sign_in.html',{'form':form})
 
 def log_in(request):
-    return render(request,'log_in.html')
+    if request.method =='POST':
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            pseudo = form.cleaned_data['pseudo']
+            mot_de_passe = form.cleaned_data['mot_de_passe']
+            #Vérif de données
+            try:
+                utilisateur = Utilisateur.objects.get(pseudo=pseudo)
+                if check_password(mot_de_passe, utilisateur.mot_de_passe):
+                    request.session['utilisateur_pseudo'] = utilisateur.pseudo
+                    request.session['utilisateur_id'] = utilisateur.id
+                    messages.success(request, "Connexion réussie.")
+                    return redirect('index')
+                else:
+                    messages.error(request, "Mot de passe incorrect.")
+            except Utilisateur.DoesNotExist:
+                messages.error(request, "Pseudo incorrect ou inexistant.")
+    else:
+        form = LogInForm()
+    return render(request,'log_in.html', {'form':form})
 
